727a65356bcad22ade47259d55e45d9283ec1416
Kelly Hansen <kellyhansen182@gmail.com>
2021-03-29 16:12:20 -0700

    lodash compose/pipe

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { compose, pipe } from "lodash/fp";
+
 let input = "   JavaScript   ";
 let output = "<div>" + input.trim() + "</div>";
 
@@ -5,4 +7,5 @@ const trim = str => str.trim();
 const wrapInDiv = str => `<div>${str}</div>`;
 const toLowerCase = str => str.toLowerCase();
 
-const result = wrapInDiv(toLowerCase(trim(input)));
+const transform = pipe(trim, toLowerCase, wrapInDiv);
+console.log(transform(input));
