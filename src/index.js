48624d6eb0cb5ee7fa30f122844782aa04c01bc2
Kelly Hansen <kellyhansen182@gmail.com>
2021-03-29 16:59:51 -0700

    immutable package

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-const numbers = [1, 2, 3];
-
-const index = numbers.indexOf(2);
-const added = [
-  ...numbers.slice(0, index),
-  4,
-  ...numbers.slice(index)
-];
-
-const removed = numbers.filter(n => n !== 2);
-
-const updated = numbers.map(n => n === 2 ? 20 : n);
-console.log(updated);
+import { Map } from 'immutable';
