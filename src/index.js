cc71e3b0b843b8e8196ebe9a12bd78c8cfc8c083
Kelly Hansen <kellyhansen182@gmail.com>
2021-03-29 16:51:33 -0700

    array methods

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-const person = {name: "John"};
-const updated = {
-  ...person,
-  address: {
-    ...person.address,
-    city: "New York"
-  },
-  name: "Bob"
-};
+const numbers = [1, 2, 3];
+
+const index = numbers.indexOf(2);
+const added = [
+  ...numbers.slice(0, index),
+  4,
+  ...numbers.slice(index)
+];
+
+const removed = numbers.filter(n => n !== 2);
+
+const updated = numbers.map(n => n === 2 ? 20 : n);
 console.log(updated);
