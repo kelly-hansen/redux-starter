d42e0513728164f37c18e7986279af350f21e16d
Kelly Hansen <kellyhansen182@gmail.com>
2021-03-30 15:29:02 -0700

    create custom store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-import store from './store';
-import { bugAdded, bugResolved } from './actions';
+import store from './customStore';
 
-const unsubscribe = store.subscribe(() => {
-  console.log('Store changed!', store.getState());
-});
-
-store.dispatch(bugAdded('Bug 1'));
-
-store.dispatch(bugResolved(1));
-
-unsubscribe();
-
-console.log(store.getState());
+console.log(store);
