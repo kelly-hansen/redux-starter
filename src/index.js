1b518ef52c0ea23ca1a096b0c9a6d50ada1d0445
Kelly Hansen <kellyhansen182@gmail.com>
2021-03-30 14:42:11 -0700

    actions in exported function in actions.js module

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import store from './store';
-import * as actions from './actionTypes';
+import { bugAdded } from './actions';
 
 const unsubscribe = store.subscribe(() => {
   console.log('Store changed!', store.getState());
 });
 
-store.dispatch({
-  type: actions.BUG_ADDED,
-  payload: {
-    description: 'Bug1'
-  }
-});
+store.dispatch(bugAdded);
 
 unsubscribe();
-
-store.dispatch({
-  type: actions.BUG_REMOVED,
-  payload: {
-    id: 1
-  }
-});
 
 console.log(store.getState());
