79b752e587d58f3ac247652e4995ba00f25bf922
Kelly Hansen <kellyhansen182@gmail.com>
2021-03-31 13:35:35 -0700

    Add reducer to bugs.js

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,7 +1,7 @@
 // Action types
-export const BUG_ADDED = "bugAdded";
-export const BUG_REMOVED = "bugRemoved";
-export const BUG_RESOLVED = "bugResolved";
+const BUG_ADDED = "bugAdded";
+const BUG_REMOVED = "bugRemoved";
+const BUG_RESOLVED = "bugResolved";
 
 // Action creators
 export function bugAdded(description) {
@@ -28,5 +28,28 @@ export function bugResolved(id) {
     payload: {
       id
     }
+  }
+}
+
+// Reducer
+let lastId = 0;
+
+export default function reducer(state = [], action) {
+  switch (action.type) {
+    case BUG_ADDED:
+      return [
+        ...state,
+        {
+          id: ++lastId,
+          description: action.payload.description,
+          resolved: false
+        }
+      ];
+    case BUG_REMOVED:
+      return state.filter(bug => bug.id !== action.payload.id);
+    case BUG_RESOLVED:
+      return state.map(bug => bug.id !== action.payload.id ? bug : { ...bug, resolved: true });
+    default:
+      return state;
   }
 }
