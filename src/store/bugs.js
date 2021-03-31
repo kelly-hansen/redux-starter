413370096d37b95c38549d19b0aaa4d905e7c4af
Kelly Hansen <kellyhansen182@gmail.com>
2021-03-31 13:34:23 -0700

    Add actions to bugs.js

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -2,3 +2,31 @@
 export const BUG_ADDED = "bugAdded";
 export const BUG_REMOVED = "bugRemoved";
 export const BUG_RESOLVED = "bugResolved";
+
+// Action creators
+export function bugAdded(description) {
+  return {
+    type: BUG_ADDED,
+    payload: {
+      description
+    }
+  }
+}
+
+export function bugRemoved(id) {
+  return {
+    type: BUG_REMOVED,
+    payload: {
+      id
+    }
+  }
+}
+
+export function bugResolved(id) {
+  return {
+    type: BUG_RESOLVED,
+    payload: {
+      id
+    }
+  }
+}
