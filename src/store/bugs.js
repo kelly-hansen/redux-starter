41783d258ac40e701da5f00b33a280300f2745ec
Kelly Hansen <kellyhansen182@gmail.com>
2021-03-31 13:52:37 -0700

    Exercise using createAction()

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,3 +1,8 @@
+import { createAction } from '@reduxjs/toolkit';
+
+const action = createAction("bugUpdated");
+console.log(action({ id: 1 }));
+
 // Action types
 const BUG_ADDED = "bugAdded";
 const BUG_REMOVED = "bugRemoved";
