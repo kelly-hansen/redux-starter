32da7f690ccf5fb152fd60c5f26ebae0d1666ac2
Kelly Hansen <kellyhansen182@gmail.com>
2021-04-01 11:12:55 -0700

    Clear index.js and create indexPriorExercises.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,3 @@
 import configureStore from './store/configureStore';
-import { bugAssignedToUser, bugAdded, bugResolved, selectUnresolvedBugs, selectBugsByUser } from './store/bugs';
-import { projectAdded } from './store/projects';
-import { userAdded } from './store/users';
-
 
 const store = configureStore();
-
-store.subscribe(() => {
-  console.log('Store changed!');
-});
-
-store.dispatch(projectAdded({ name: 'My First Project' }));
-store.dispatch(bugAdded({ description: 'Bug 1' }));
-store.dispatch(bugAdded({ description: 'Bug 2' }));
-store.dispatch(bugAdded({ description: 'Bug 3' }));
-store.dispatch(bugResolved({ id: 1 }));
-
-store.dispatch(userAdded({ name: 'Trevor' }));
-
-store.dispatch(bugAssignedToUser({ bugId: 2, userId: 1 }));
-store.dispatch(bugAssignedToUser({ bugId: 3, userId: 1 }));
-
-const unresolvedBugs = selectUnresolvedBugs(store.getState());
-console.log('unresolvedBugs:', unresolvedBugs);
-
-const user1Bugs = selectBugsByUser(1)(store.getState());
-console.log('user1Bugs:', user1Bugs);
