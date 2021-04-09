6f287f527dc2a9d008c86d58028324e460fad21b
Kelly Hansen <kellyhansen182@gmail.com>
2021-04-08 20:56:51 -0700

    Access StoreContext in Bugs component

--- bugs-frontend/src/components/bugs.jsx
+++ bugs-frontend/src/components/bugs.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import StoreContext from '../contexts/storeContext';
 
 export default class Bugs extends Component {
+  static contextType = StoreContext;
+
+  componentDidMount() {
+    console.log(this.context);
+  }
+
   render() {
     return <div>Bugs</div>
   }
