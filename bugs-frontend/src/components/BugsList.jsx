e29a9845214d48ba0f5cb298477132161c162066
Kelly Hansen <kellyhansen182@gmail.com>
2021-04-09 09:48:33 -0700

    Implement Resolve button exercise

--- bugs-frontend/src/components/BugsList.jsx
+++ bugs-frontend/src/components/BugsList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { loadBugs, selectUnresolvedBugs } from '../store/bugs';
+import { loadBugs, selectUnresolvedBugs, resolveBug } from '../store/bugs';
 
 const BugsList = () => {
   const dispatch = useDispatch();
@@ -13,7 +13,10 @@ const BugsList = () => {
   return (
     <ul>
       {bugs.map(bug => (
-        <li key={bug.id}>{bug.description}</li>
+        <li key={bug.id}>
+          {bug.description}
+          <button onClick={() => dispatch(resolveBug(bug.id))}>Resolve</button>
+        </li>
       ))}
     </ul>
   );
