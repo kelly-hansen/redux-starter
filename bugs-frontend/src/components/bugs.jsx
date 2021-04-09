066f120bce6f73587db74129309af007cb2d499c
Kelly Hansen <kellyhansen182@gmail.com>
2021-04-08 21:01:59 -0700

    Subscribe to store in Bugs component

--- bugs-frontend/src/components/bugs.jsx
+++ bugs-frontend/src/components/bugs.jsx
@@ -4,8 +4,21 @@ import StoreContext from '../contexts/storeContext';
 export default class Bugs extends Component {
   static contextType = StoreContext;
 
+  state = { bugs: [] };
+
   componentDidMount() {
-    console.log(this.context);
+    const store = this.context;
+
+    this.unsubscribe = store.subscribe(() => {
+      const bugsInStore = store.getState().entities.bugs.list;
+      if (this.state.bugs !== bugsInStore) {
+        this.setState({ bugs: bugsInStore });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
   }
 
   render() {
