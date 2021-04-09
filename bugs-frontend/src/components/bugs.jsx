1042fb18a17c2ce0a336fc0912199ce5ec76d764
Kelly Hansen <kellyhansen182@gmail.com>
2021-04-08 21:19:23 -0700

    Use React-Redux to simplify store provider/access

--- bugs-frontend/src/components/bugs.jsx
+++ bugs-frontend/src/components/bugs.jsx
@@ -1,36 +1,33 @@
 import React, { Component } from 'react';
-import StoreContext from '../contexts/storeContext';
 import { loadBugs } from '../store/bugs';
+import { connect } from 'react-redux';
 
-export default class Bugs extends Component {
-  static contextType = StoreContext;
-
-  state = { bugs: [] };
+class Bugs extends Component {
 
   componentDidMount() {
-    const store = this.context;
-
-    this.unsubscribe = store.subscribe(() => {
-      const bugsInStore = store.getState().entities.bugs.list;
-      if (this.state.bugs !== bugsInStore) {
-        this.setState({ bugs: bugsInStore });
-      }
-    });
-
-    store.dispatch(loadBugs());
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
+    this.props.loadBugs();
   }
 
   render() {
     return (
       <ul>
-        {this.state.bugs.map(bug => (
+        {this.props.bugs.map(bug => (
           <li key={bug.id}>{bug.description}</li>
         ))}
       </ul>
     );
   }
 }
+
+const mapStateToProps = state => ({
+  bugs: state.entities.bugs.list
+});
+
+const mapDispatchToProps = dispatch => ({
+  loadBugs: () => dispatch(loadBugs())
+});
+
+// Container
+//  Presentation (Bugs)
+
+export default connect(mapStateToProps, mapDispatchToProps)(Bugs)
