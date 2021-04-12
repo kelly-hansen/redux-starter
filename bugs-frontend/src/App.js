d82fa6c9375920d9ca60569d4969bb1a3aa2cdbe
Kelly Hansen <kellyhansen182@gmail.com>
2021-04-12 11:38:44 -0700

    Import and wrap components in App with Router

--- bugs-frontend/src/App.js
+++ bugs-frontend/src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import BugsList from "./components/BugsList";
 import configureStore from './store/configureStore';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const store = configureStore();
 
 function App() {
   return (
     <Provider store={store}>
-      <BugsList />
+      <Router>
+        <BugsList />
+      </Router>
     </Provider>
   );
 }
