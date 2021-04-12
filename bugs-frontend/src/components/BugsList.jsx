5b2ab8281b239601ddfd8a5e8c72626c6f3ffe33
Kelly Hansen <kellyhansen182@gmail.com>
2021-04-12 11:55:06 -0700

    Link to Home on BugsList

--- bugs-frontend/src/components/BugsList.jsx
+++ bugs-frontend/src/components/BugsList.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { loadBugs, selectUnresolvedBugs, resolveBug } from '../store/bugs';
 
@@ -11,14 +12,17 @@ const BugsList = () => {
   }, []);
 
   return (
-    <ul>
-      {bugs.map(bug => (
-        <li key={bug.id}>
-          {bug.description}
-          <button onClick={() => dispatch(resolveBug(bug.id))}>Resolve</button>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {bugs.map(bug => (
+          <li key={bug.id}>
+            {bug.description}
+            <button onClick={() => dispatch(resolveBug(bug.id))}>Resolve</button>
+          </li>
+        ))}
+      </ul>
+      <Link to="/">Back to Home</Link>
+    </>
   );
 }
 
